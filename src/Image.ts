54a1c3cf0de66ddb9e91dfed7357e1a0d4bb1d30
snowcxt <snowcxt@gmail.com>
2017-03-01 15:19:45 -0800

    add mask for Image

--- src/Image.ts
+++ src/Image.ts
@@ -2,6 +2,7 @@ import {Shape, IShapeConfig} from "./Core";
 
 export interface IImageConfig extends IShapeConfig {
     image: ImageTexture;
+    mask?: PIXI.Graphics;
 }
 
 export class ImageTexture {
@@ -12,70 +13,22 @@ export class ImageTexture {
     constructor(src) {
         this.src = src;
         this.texture = PIXI.Texture.fromImage(src);
-        // this.onLoaded = (onTextureUpdate: (image: ImageTexture) => void) => {
-        //    this.texture.addEventListener("update", () => { onTextureUpdate(this); });
-        // };
-    }
-
-    // public load(onloaded: (texture: ImageTexture) => void= () => { }) {
-    //    this.texture = PIXI.Texture.fromImage(this.src);
-    //    this.texture.addEventListener("update", () => { alert(" onloaded(this)"); });
-    //    onloaded(this);
-    // }
-
-
-    static loadAll(images, loaded: () => void = () => { }) {
-        //var imageLoader = new PIXI.ImageLoader("http://www.goodboydigital.com/pixijs/logo_small.png", true);
-        //imageLoader.addEventListener("loaded", onComplete);
-        //imageLoader.load();
-        //renderer.render(stage);
-
-
-        //function onComplete(e) {
-        //    console.log("event data: ", e);
-        //    var bunny = new PIXI.Sprite(e.content.texture);
-
-        //    bunny.position = new PIXI.Point(200, 150);
-        //    stage.addChild(bunny);
-
-        //    renderer.render(stage);
-        //}
-
-        //if (images) {
-        //    var count = 0;
-        //    if (typeof (images) == 'object') {
-        //        for (var key in images) {
-        //            count++;
-        //            var image: ImageTexture = images[key];
-        //            image.onLoaded(() => {
-        //                count--;
-        //                if (count == 0) {
-        //                    loaded();
-        //                }
-        //            });
-        //        }
-        //    } else if (images instanceof Array) {
-        //        count = (<Array<Pixis.ImageTexture>>images).length;
-        //        (<Array<Pixis.ImageTexture>>images).forEach((img) => {
-        //            img.onLoaded(() => {
-        //                count--;
-        //                if (count == 0) {
-        //                    loaded();
-        //                }
-        //            });
-        //        });
-        //    }
-        // } else {
-        //    loaded();
-        // }
     }
 }
 
 export class Image extends Shape {
-    // private   texture=null;
+    shape: PIXI.Sprite;
+    public attrs: IImageConfig;
     public onLoaded: (onTextureUpdate) => void;
 
     constructor(config: IImageConfig) {
         super(new PIXI.Sprite(config.image.texture), config);
+    }
+
+    draw() {
+        super.draw();
+        if (this.attrs.mask) {
+            this.shape.mask = this.attrs.mask;
+        }
     }
 }
